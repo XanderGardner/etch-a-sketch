eb39b62328b668118e8727e8d13c0d07007f1f51
Xander Gardner <xandergardner7@gmail.com>
2020-12-17 23:44:39 -0500

    added functionality

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,29 +1,66 @@
-// add content to grid
+let numRows = 16;
+
+// constants
+const gridPx = 40 * 16;
 const grid = document.getElementById("grid");
-for (let i = 0; i < 16*16; i++) {
-    const div = document.createElement('div');
-    div.setAttribute("class", "item");
-    grid.appendChild(div);
+setup(numRows);
+
+// setup
+function setup(numRows) {
+    setSizing(numRows);
+    addContent();
+    let gridItems = document.querySelectorAll(".item");
+    setBackground(gridItems);
+    hoverAdd(gridItems);
+}
+
+// shake button
+const shakeBtn = document.getElementById("shake");
+shakeBtn.addEventListener("click", () => {
+    const newRowsStr = prompt("How many rows/columns? (Enter between 1 and 100)");
+    numRows = parseInt(newRowsStr);
+    setup(numRows);
+});
+
+// helper functions
+
+// change sizing
+function setSizing(numRows) {
+    const pxPer = gridPx / numRows;
+    const styleCol = "grid-template-columns: repeat("+numRows+", "+pxPer+"px);";
+    const styleRow = " grid-template-rows: repeat("+numRows+", "+pxPer+"px)";
+    grid.setAttribute("style", styleCol + styleRow);
+}
+
+// add content to grid
+function addContent() {
+    for (let i = 0; i < numRows * numRows; i++) {
+        const div = document.createElement('div');
+        div.setAttribute("class", "item");
+        grid.appendChild(div);
+    }
 }
 
 // set item background colors
-const gridItems = document.querySelectorAll(".item");
-gridItems.forEach((item) => {
-    item.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
-});
-
-// update items that are hovered over
-gridItems.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-        let opacity = Number(item.style.backgroundColor.slice(-4, -1));
-        if (isNaN(opacity)) {
-            opacity = Number(item.style.backgroundColor.slice(-3, -1));
-        }
-        if (opacity < 0.9) {
-            opacity += 0.1;
-        }
-        console.log(opacity);
-        item.style.backgroundColor = "rgba(0, 0, 0, " + opacity + ")";
+function setBackground(gridItems) {
+    gridItems.forEach((item) => {
+        item.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
     });
-});
+}
 
+// add updating items that are hovered over
+function hoverAdd(gridItems) {
+    gridItems.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            let opacity = Number(item.style.backgroundColor.slice(-4, -1));
+            if (isNaN(opacity)) {
+                opacity = Number(item.style.backgroundColor.slice(-3, -1));
+            }
+            if (opacity < 0.9) {
+                opacity += 0.1;
+            }
+            console.log(opacity);
+            item.style.backgroundColor = "rgba(0, 0, 0, " + opacity + ")";
+        });
+    });
+}
